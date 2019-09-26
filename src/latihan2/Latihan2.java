5323e0c7790021e6c51b9bc054e35acecd59e90f
Riski Dwi <bintangmalamhari@gmail.com>
2019-09-26 12:43:30 +0700

    Add files via upload

---
+++ src/latihan2/Latihan2.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2;
+
+/**
+ *
+ * @author dwisr
+ * NAMA      : RISKI DWI SABARIYANTO
+ * KELAS     : IF -1 
+ * NIM       : 10118026
+ * Deskripsi : Program untuk menampilkan isi variabel
+ * 
+ */
+public class Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // deklarasi variable
+        int nilaiInt;
+        final double PHI = 3.4;//konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //memberi nilai ke variable
+        nilaiInt = 21;
+        nilaiLogika = true;
+        nilaiKarakter = 'E';
+        
+        //menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi Variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = "+ nilaiLogika);
+        System.out.println("Isi Variabel Karakter = " + nilaiKarakter);
+        
+    }
+    
+}
